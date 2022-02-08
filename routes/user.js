d75ee6c4abb49021f44ad9d55bb0fd7df7dd94e7
Max <burnashkin02@mail.ru>
2022-02-08 20:38:24 +0900

    Servak i ruchki plus sessiya

---
+++ routes/user.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const {
+  checkUserAndCreateSession,
+  createUserAndSession, destroySession,
+  isValid,
+  renderSignInForm,
+  renderSignUpForm,
+  renderProfile
+} = require("../controllers/userControllers");
+
+const router = express.Router();
+
+router
+  .route('/signup')
+  // Страница регистрации пользователя
+  .get(renderSignUpForm)
+  // Регистрация пользователя
+  .post(isValid,  createUserAndSession);
+
+router
+  .route('/signin')
+  // Страница аутентификации пользователя
+  .get(renderSignInForm)
+  // Аутентификация пользователя
+  .post(checkUserAndCreateSession);
+
+router.get('/signout', destroySession);
+router.get('/profile', renderProfile )
+
+module.exports = router;
