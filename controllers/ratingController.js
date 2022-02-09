b38cfa88f61bb429bec9af66ef9b235106729603
AndreyOhotnikov <ququruq@gmail.com>
2022-02-09 12:38:35 +0300

    ratingSort

---
+++ controllers/ratingController.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const {Way, User, Comment} = require('../db/models/');
+
+exports.ratingController = async (ways1) => {
+  return await Promise.all(ways1.map( async (el) => {
+    const arrComments = await Comment.findAll({where: {way_id: el.id}, raw:true})
+    el.rating = Number((arrComments.reduce((acc, comm) => acc += comm.rating, 0) / arrComments.length).toFixed(2)) || 'рейтинг отсутствует';
+    return el
+  }));
+};
