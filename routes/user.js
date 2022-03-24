6cf22070e606213bf95dc78f654e0b43d0ce7eff
Valeria <valeria1992.08@bk.ru>
2022-03-24 15:45:24 +0300

    Refactoring

--- routes/user.js
+++ routes/user.js
@@ -18,39 +18,31 @@ const router = express.Router();
 
 router
   .route('/signup')
-  // Страница регистрации пользователя
   .get(renderSignUpForm)
-  // Регистрация пользователя
-  .post(isValid,  createUserAndSession);
+  .post(isValid, createUserAndSession);
 
 router
   .route('/signin')
-  // Страница аутентификации пользователя
   .get(renderSignInForm)
-  // Аутентификация пользователя
   .post(checkUserAndCreateSession);
 
 router.get('/signout', destroySession);
 
 router
-  .route('/edit/:id')                          
-  // Генерация страницы, редактирования профиля
+  .route('/edit/:id')
   .get(isAuth, isRedactorProfile, renderFormEditUser)
-  // Получение данных - редактирования профиля
   .put(isAuth, isRedactorProfile, editUserProfile);
 
 router
   .route('/admin/:id')
-  // редактирование прав админа
   .put(isAdmin, EditIsAdmin);
-  
+
 router
   .route('/:id')
-  // Генерация формы информации о пользователе
   .get(isAuth, renderUserProfile);
 
 router.get('/', async (req, res) => res.redirect('/'));
-// если не введены данные то редиректим на главную страницу
+
 
 
 module.exports = router;
